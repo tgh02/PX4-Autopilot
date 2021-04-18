e7b4810909671f59adb1f6c18ab00329a52da6da
Ronzevous <76603178+ChunHawLai@users.noreply.github.com>
2021-04-18 22:57:48 +0800

    Update mc_att_control.hpp

--- src/modules/mc_att_control/mc_att_control.hpp
+++ src/modules/mc_att_control/mc_att_control.hpp
@@ -221,7 +221,7 @@ private:
 		(ParamFloat<px4::params::MC_RR_INT_LIM>) _param_mc_rr_int_lim,
 		(ParamFloat<px4::params::MC_ROLLRATE_D>) _param_mc_rollrate_d,
 		(ParamFloat<px4::params::MC_ROLLRATE_FF>) _param_mc_rollrate_ff,
-		(ParamFloat<px4::params::MC_ROLL_JM>) _param_mc_roll_jm,	// SIPIC
+		(ParamFloat<px4::params::MC_ROLL_JM>) _param_mc_roll_jm,			// SIPIC
 			
 		(ParamFloat<px4::params::MC_PITCH_P>) _param_mc_pitch_p,
 		(ParamFloat<px4::params::MC_PITCHRATE_P>) _param_mc_pitchrate_p,
@@ -229,7 +229,7 @@ private:
 		(ParamFloat<px4::params::MC_PR_INT_LIM>) _param_mc_pr_int_lim,
 		(ParamFloat<px4::params::MC_PITCHRATE_D>) _param_mc_pitchrate_d,
 		(ParamFloat<px4::params::MC_PITCHRATE_FF>) _param_mc_pitchrate_ff,
-		(ParamFloat<px4::params::MC_PITCH_JM>) _param_mc_pitch_jm,	// SIPIC
+		(ParamFloat<px4::params::MC_PITCH_JM>) _param_mc_pitch_jm,			// SIPIC
 
 		(ParamFloat<px4::params::MC_YAW_P>) _param_mc_yaw_p,
 		(ParamFloat<px4::params::MC_YAWRATE_P>) _param_mc_yawrate_p,
@@ -237,7 +237,7 @@ private:
 		(ParamFloat<px4::params::MC_YR_INT_LIM>) _param_mc_yr_int_lim,
 		(ParamFloat<px4::params::MC_YAWRATE_D>) _param_mc_yawrate_d,
 		(ParamFloat<px4::params::MC_YAWRATE_FF>) _param_mc_yawrate_ff,
-		(ParamFloat<px4::params::MC_YAW_JM>) _param_mc_yaw_jm,	// SIPIC
+		(ParamFloat<px4::params::MC_YAW_JM>) _param_mc_yaw_jm,				// SIPIC
 
 		(ParamFloat<px4::params::MC_DTERM_CUTOFF>) _param_mc_dterm_cutoff,			/**< Cutoff frequency for the D-term filter */
 
@@ -247,6 +247,10 @@ private:
 		(ParamFloat<px4::params::MC_TPA_RATE_P>) _param_mc_tpa_rate_p,				/**< Throttle PID Attenuation slope */
 		(ParamFloat<px4::params::MC_TPA_RATE_I>) _param_mc_tpa_rate_i,				/**< Throttle PID Attenuation slope */
 		(ParamFloat<px4::params::MC_TPA_RATE_D>) _param_mc_tpa_rate_d,				/**< Throttle PID Attenuation slope */
+		(ParamFloat<px4::params::MC_TPA_BREAK_K1>) _param_mc_tpa_break_k1,		// SIPIC
+		(ParamFloat<px4::params::MC_TPA_BREAK_K2>) _param_mc_tpa_break_k2,		// SIPIC		
+		(ParamFloat<px4::params::MC_TPA_RATE_K1>) _param_mc_tpa_rate_k1,		// SIPIC
+		(ParamFloat<px4::params::MC_TPA_RATE_K2>) _param_mc_tpa_rate_k2, 		// SIPIC
 
 		(ParamFloat<px4::params::MC_ROLLRATE_MAX>) _param_mc_rollrate_max,
 		(ParamFloat<px4::params::MC_PITCHRATE_MAX>) _param_mc_pitchrate_max,
